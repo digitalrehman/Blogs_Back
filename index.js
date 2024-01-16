c471ea0560ad636f0da81316b654a7a227ae627c
Rehman Arain <145242032+Digitalrehman@users.noreply.github.com>
2024-01-16 19:27:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const router = require("./router/route");
 mongoose.connect(process.env.MONGODB_URI)
   .then((res) => console.log("Connect To MongoDB"))
   .catch((err) => console.log("Inter Server Error"));
-App.use(cors());
+const corsOptions = {
+  origin: "https://blogs-app.pages.dev",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+App.use(cors(corsOptions));
+
 App.use(express.json());
 App.use("/api", router);
 App.listen(PORT, () => console.log(`Start localhost:${PORT}`));
